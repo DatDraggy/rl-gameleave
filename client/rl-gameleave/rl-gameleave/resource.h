8bd00972ba9ccf2da25e3487ac347748066a6cf6
DatDraggy <bmxstreet@gmx.net>
2019-02-15 20:58:07 +0100

    something i guess

---
+++ client/rl-gameleave/rl-gameleave/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+
+#include <windows.h>
+#include <shellapi.h>
+
+#define ICO1 101
+#define ID_TRAY_APP_ICON    1001
+#define ID_TRAY_EXIT        1002
+#define WM_SYSICON          (WM_USER + 1)
